6c3eee0ec20c87fcc7cb066d700b14f72c856169
Ilan Kroter <44408485+IlanKro@users.noreply.github.com>
2020-05-07 13:55:25 +0300

    fixed identations

--- src/user_managment.js
+++ src/user_managment.js
@@ -15,18 +15,17 @@ function userType(val) {
     var img_upload = document.getElementById("imgup")
     var bank = document.getElementById("bank_account")
     if (val == "renter") {                       
-            img_upload.style.display = "none"         
-            bank.style.display = "block"
-            img_upload.setAttribute("required","false")
-            bank.input.setAttribute("required","true")
+        img_upload.style.display = "none"         
+        bank.style.display = "block"
+        //img_upload.setAttribute("required","false")
+        //bank.input.setAttribute("required","true")
     }
     if (val == "student") {           
-            img_upload.style.display="block"           
-            bank.style.display = "none"
-            img_upload.input.setAttribute("required","true")
-            bank.input.setAttribute("required","false")
-            
-        }
+        img_upload.style.display="block"           
+        bank.style.display = "none"
+        //img_upload.input.setAttribute("required","true")
+        //bank.input.setAttribute("required","false")            
+    }
 }
 
 
