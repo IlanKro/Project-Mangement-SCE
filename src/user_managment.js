9e7f99d7408c3a5e66bfd417753147c4d325c390
Ilan Kroter <44408485+IlanKro@users.noreply.github.com>
2020-05-07 13:43:01 +0300

    finished the login html, started the javascript for the retrival of data, need to switch branches

---
+++ src/user_managment.js
@@ -0,0 +1,32 @@
+function login() {
+    var username = document.getElementById("ue").value
+    var password = document.getElementById("pass").value   
+    // add link to database.       
+    try{window.location="homepage_student.html"}
+    catch(e) {window.location="404.html"}    
+}
+
+
+function signUp() {
+    
+}
+
+function userType(val) { 
+    var img_upload = document.getElementById("imgup")
+    var bank = document.getElementById("bank_account")
+    if (val == "renter") {                       
+            img_upload.style.display = "none"         
+            bank.style.display = "block"
+            img_upload.setAttribute("required","false")
+            bank.input.setAttribute("required","true")
+    }
+    if (val == "student") {           
+            img_upload.style.display="block"           
+            bank.style.display = "none"
+            img_upload.input.setAttribute("required","true")
+            bank.input.setAttribute("required","false")
+            
+        }
+}
+
+
