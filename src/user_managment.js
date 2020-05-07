d77063006728ae9e833f0bbb69b64c9e43726cfc
Ilan Kroter <44408485+IlanKro@users.noreply.github.com>
2020-05-07 16:03:46 +0300

    changed the login logic into a class with static methods to remove global functions, also changed the documentation.. classes are with pascalcase

--- src/user_managment.js
+++ src/user_managment.js
@@ -1,31 +1,41 @@
-function login() {
-    var username = document.getElementById("ue").value
-    var password = document.getElementById("pass").value   
-    // add link to database.       
-    try{window.location="homepage_student.html"}
-    catch(e) {window.location="404.html"}    
-}
 
+class UserManagement {
+    static login() {
+        var username = document.getElementById("ue").value
+        var password = document.getElementById("pass").value        
+        try{window.location = "homepage_student.html"}
+        catch(e) {window.location = "404.html"} 
+        UserManagement.empty(username)
+        UserManagement.empty(password)
+    }
 
-function signUp() {
+    static signUp() {
+        alert("HELLO!")
+    }
+
+    static userType(val) {         
+        var img_upload = document.getElementById("imgup")        
+        var bank = document.getElementById("bank_account")        
+        if (val == "renter") {                       
+            img_upload.style.display = "none"         
+            bank.style.display = "block"
+            //img_upload.setAttribute("required","false")
+            //bank.input.setAttribute("required","true")
+        }
+        if (val == "student") {           
+            img_upload.style.display = "block"           
+            bank.style.display = "none"
+            //img_upload.input.setAttribute("required","true")
+            //bank.input.setAttribute("required","false")            
+        }
+    }
     
+    static empty(a) {
+        return a
+        //temporarly used to satisfy the linter will be deleted later
+    }
+
 }
 
-function userType(val) { 
-    var img_upload = document.getElementById("imgup")
-    var bank = document.getElementById("bank_account")
-    if (val == "renter") {                       
-        img_upload.style.display = "none"         
-        bank.style.display = "block"
-        //img_upload.setAttribute("required","false")
-        //bank.input.setAttribute("required","true")
-    }
-    if (val == "student") {           
-        img_upload.style.display="block"           
-        bank.style.display = "none"
-        //img_upload.input.setAttribute("required","true")
-        //bank.input.setAttribute("required","false")            
-    }
-}
-
-
+var u =new UserManagement()
+u.empty(1)
