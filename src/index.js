69efb12008ba69ee5bcf94403d49f8bd4ae57e72
Ilan Kroter <44408485+IlanKro@users.noreply.github.com>
2020-05-11 23:42:54 +0300

    working sign up work on login, and add data to user

--- src/index.js
+++ src/index.js
@@ -2,15 +2,40 @@ const express  = require("express")
 const app_port = process.env.PORT || 3000
 const app = express()
 const user_controller= require("./userController")
-
+const admin= require("./firebase")()
+//const admin=admin1()
 app.set("view engine", "ejs")
-app.use(express.static("css") )
+
+app.use("/css",express.static("css"))
 app.use(express.static("images") )
 
 app.get("/",(req, res) => {
-    res.render("index")
+    res.render("index", {title: "hello user"})
 })
-user_controller(app)
+console.log(admin.app().name)
+/*
+admin.auth().createUser({
+  email: 'user@example.com',
+  emailVerified: false,
+  phoneNumber: '+11234567890',
+  password: 'secretPassword',
+  displayName: 'John Doe',
+  disabled: false
+})
+*/
+
+admin.auth().getUser("OqKjlolzjQPTDDKCzxYhfiLR4wI2")
+  .then(function(userRecord) {
+    // See the UserRecord reference doc for the contents of userRecord.
+    console.log('Successfully fetched user data:', userRecord.toJSON());
+  })
+  .catch(function(error) {
+    console.log('Error fetching user data:', error);
+});
+
+
+
+user_controller(app,admin)
 app.listen(app_port)
 console.log(`app is running. port: ${app_port}`)
 console.log(`http://127.0.0.1:${app_port}/`)
--- src/userController.js
+++ src/userController.js
@@ -1,6 +1,6 @@
 const body_parser=require("body-parser")
 const body_url=body_parser.urlencoded({extended: "false"})
-module.exports = function(app) {
+module.exports = function(app,admin) {
 
     app.get("/login",(req, res) => {
         res.render("login")
@@ -16,11 +16,29 @@ module.exports = function(app) {
 
     app.post("/login",body_url, (req,res) => {
         console.log(req.body)
-        console.log("hello")
+        let email= req.body.email
+        let password= req.body.password
+        //TODO: make this code below to work
+        /*
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+            // Handle Errors here.
+            window.alert("Error : " + error.message)
+        })
+        */
     })
-
     app.post("/signup",body_url, (req,res) => {
         console.log(req.body)
+        console.log(req.body.email + req.body.pass + " " )
+          //actual name: req.body.fname + " " + req.body.lname,
+          //TODO:find a way to add those 2, the image, the bank account and USER TYPE!!
+        admin.auth().createUser({
+          email: req.body.email,
+          emailVerified: false,
+          password: req.body.password,
+          disabled: false,
+          displayName: req.body.username
+        })
+
     })
 
     app.post("/forgotpassword",body_url, (req,res) => {
