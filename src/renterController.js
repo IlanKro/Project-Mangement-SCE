4a21ebf77ef4a8ba29e77671a9260487331c3a6e
Ilan Kroter <44408485+IlanKro@users.noreply.github.com>
2020-05-16 12:26:40 +0300

    Renter2 (#17)
    * can now add an apartment for rent
    
    * deleted HTML files (they were reduntant and unused), and made changes to signup logic.. they will be refactored next sprint)

--- src/renterController.js
+++ src/renterController.js
@@ -1,7 +1,6 @@
 const body_parser=require("body-parser")
 const body_url=body_parser.urlencoded({extended: "true"})
 const body_json=body_parser.json()
-const pass_check=require("check-password-strength")
 
 module.exports = function(app,admin) {
     const database= admin.firestore()
@@ -14,8 +13,8 @@ module.exports = function(app,admin) {
             return data
         })
     }
-    app.get("/homepage_admin",(req, res) => {
-        Promise.all([getLibrary("Units"),getLibrary("Users"),getLibrary("Reviews")]).then(data =>
+    app.get("/homepage_renter",(req, res) => {
+        Promise.all([getLibrary("Units"),getLibrary("Users"),getLibrary("Reviews"),getLibrary("Orders")]).then(data =>
         {
             let units=data[0]
             let users=data[1]
@@ -27,50 +26,58 @@ module.exports = function(app,admin) {
                 console.log(users[i])
             }
             */
-            res.render("homepage_admin",{"units" :units,"users" : users,"reviews" :reviews})
+            res.render("homepage_renter",{"units" :units,"users" : users,"reviews" :reviews})
         })
     })
 
-    app.post("/homepage_admin/enable",body_url, (req,res) => {
+    app.post("/homepage_renter/post_housing_unit",body_url, (req,res) => {
+        req.body.available= true
+        req.body.student= null
         console.log(req.body)
-        console.log(req.body.uid)
-        admin.auth().updateUser(req.body.uid, {disabled: false})
-            .then(function(userRecord) {
-                console.log("Successfully enabled user data:", userRecord.toJSON())
-                res.render("success_page",{"message" : userRecord.email + " activated!"} )
+        database.collection("Users").doc(req.body.user_id).get().then(ref => {
+            req.body.user_id=ref.toString()
+            console.log(ref)
+            console.log(ref._path)
+            console.log(ref.toString())
+            database.collection("Units").add(req.body).then(() => {
+                res.render("message_page",{"message" : req.body.city + " "
+                + req.body.street + " " + req.body.house_number + " available for rent!"} )
             })
-            .catch(function(error) {
-                console.log("Error fetching user data:", error)
-                res.render("message_page",{"message" : error})
-            })
+        }).catch((error) => {
+            res.render("message_page",{"message" : error})
+        })
+
+
+
     })
+    /*
 
     app.post("/homepage_admin/ban",body_url, (req,res) => {
         admin.auth().updateUser(req.body.uid, {disabled: true})
             .then(function(userRecord) {
                 console.log("Successfully enabled user data:", userRecord.toJSON())
-                res.render("message_page",{"message" : userRecord.email + " banned!"} )
+                res.render("success_page",{"success" : userRecord.email + " banned!"} )
             })
             .catch(function(error) {
                 console.log("Error fetching user data:", error)
-                res.render("message_page",{"message" : error})
+                res.send(error)
             })
     })
     app.post("/homepage_admin/unban",body_url, (req,res) => {
         admin.auth().updateUser(req.body.uid, {disabled: false})
             .then(function(userRecord) {
                 console.log("Successfully enabled user data:", userRecord.toJSON())
-                res.render("message_page",{"message" : userRecord.email + " activated!"} )
+                res.render("success_page",{"success" : userRecord.email + " activated!"} )
             })
             .catch(function(error) {
                 console.log("Error fetching user data:", error)
-                res.render("message_page",{"message" : error})
+                res.send(error)
             })
     })
     app.post("/homepage_admin/rmv_review",body_url, (req,res) => {
         //console.log(req.body)
-        res.render("message_page",{"message" : req.body.rid + " deleted!"} )
+        res.render("success_page",{"success" : req.body.rid + " deleted!"} )
     })
-
+*/
 
 }
