6546b350a5676eeaad2af43baeed9ae011735fd3
Ilan Kroter <44408485+IlanKro@users.noreply.github.com>
2020-05-29 16:47:05 +0300

    edit posts implemented successfully

--- src/pageScripts/edit.js
+++ src/pageScripts/edit.js
@@ -1,33 +1,51 @@
-window.onload=function(){
-    document.getElementById("addHousingForm").reset
-}
-document.querySelector("#addHousingForm").addEventListener("submit", (e) => {
+var images_to_delete= []
+document.querySelector("#editHousingForm").addEventListener("submit", (e) => {
     // handle second (inner) form for payment.
     e.preventDefault()
-    let form=document.getElementById("addHousingForm").elements
+    let form=document.getElementById("editHousingForm").elements
     let housing_data= {}
     for(let i=0;i<form.length-1;i++){
         let element=form.item(i)
         housing_data[element.name]=element.value
     }
-    if(fileList[0].name == undefined) {
-        alert("Cannot upload a housing unit without at least one photo!")
-        location.reload()
+    let images= [] //this array is the images which will be saved to database.
+    for (let i=0; i<housing_data["images_num"];i++) {
+        if(!images_to_delete[i])
+            images.push(housing_data["image" + i])
+        delete housing_data["image" + i] //remove it from the hidden parameter.
+    }
+    delete housing_data["images_num"] // deleting this as it's no longer needed.
+    if (images.length== 0 && housing_data["images"]=="") { //if no photo is left..
+        alert("You cannot have a housing unit without photos")
         return false
     }
-    document.getElementById("postHouseButton").disabled= true //disables the button so they wait for the upload to finish.
-    document.getElementById("postHouseButton").value = "Posting..."
+    document.getElementById("editHouseButton").disabled= true //disables the button so they wait for the upload to finish.
+    document.getElementById("editHouseButton").value = "Updating..."
     let image_uploads= []
     Array.prototype.forEach.call(fileList,(image,imgnum) => {
         image_uploads[imgnum]=uploadImage(imgnum,housing_data["city"]+housing_data["street"]+ housing_data["house_number"])
     })
 
     Promise.all(image_uploads).then((uploads)=> {
-        housing_data["images"]=uploads //set images as the download link urls.
-        sendJSON("/homepage_renter/post_housing_unit",housing_data)
+        //join the old pictures with the new ones.  
+        housing_data["images"]=images.concat(uploads) //set images as the download link urls.
+        sendJSON("/homepage_renter/edit_housing_unit",housing_data)
     })
 
+
 })
+
+document.querySelector("#deleteImage").addEventListener("submit", (e) => {
+    // handle second (inner) form for payment.
+    e.preventDefault()
+    let form=document.getElementById("deleteImage").elements
+    for(let i=0;i<form.length-1;i++){
+        let to_delete= form.item(i).checked //go through the checkboxes
+        images_to_delete[i]=to_delete
+        //select those that are to be deleted.
+    }
+})
+
 
 function sendJSON(url,data) {
     // send to url the json data
@@ -36,8 +54,8 @@ function sendJSON(url,data) {
     request.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
     request.onload = function (e) {
         alert(request.responseText)
-        document.getElementById("postHouseButton").value = "Post"
-        document.getElementById("postHouseButton").disabled= false //enables the button again.
+        document.getElementById("editHouseButton").value = "Edit"
+        document.getElementById("editHouseButton").disabled= false //enables the button again.
     }
     request.send(JSON.stringify(data))
 }
@@ -48,6 +66,7 @@ fileSelector.addEventListener("change", (event) => {
     //add listener to the image upload selector.
     fileList = event.target.files
 })
+
 var tasks=[]
 async function uploadImage(imgnum,address) {
     //uploads an image with the address name.
