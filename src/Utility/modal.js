16986a13a671e6e5a454a6478182c42771788265
Ilan Kroter <44408485+IlanKro@users.noreply.github.com>
2020-05-30 20:12:57 +0300

    Edit renter (#35)
    * edit posts implemented successfully
    
    * now you can accept or reject orders as renter
    
    * added attraction adding to the renter
    
    * finished the attractions! also fixed 2 bugs I made :D

--- src/Utility/modal.js
+++ src/Utility/modal.js
@@ -16,6 +16,7 @@ for(let i=0; i<modals.length;i++) {
     }
 }
 // When the user clicks anywhere outside of the modal, close it
+
 window.onclick = function(event) {
     for(let i=0;i<modals.length;i++) {
         if (event.target == modals[i]) {
--- src/controller/renterController.js
+++ src/controller/renterController.js
@@ -123,7 +123,47 @@ module.exports = function(app,admin) {
             res.send(err)
         })
     })
-  //Order control:
+
+    app.post("/homepage_renter/accept_order",body_url, (req,res) => {
+        console.log(req.body)
+        database.collection("Units").doc(req.body.unit_id).update({
+            "available" : false, //no longer available
+            "student" : database.collection("Users").doc(req.body.student_id) //create reference
+        }).then ((success) => {
+            //database.collection("Units").doc(req.body.order_id)
+            database.collection("Orders").doc(req.body.order_id).update({
+                "accepted": true
+            }).then((success) =>{
+                res.render("message_page",{"message": "accepted"})
+            })
+        }).catch((err) => {
+            console.log(err)
+            res.render("message_page",{"message": err})
+        })
+
+
+    })
+
+    app.post("/homepage_renter/reject_order",body_url, (req,res) => {
+        database.collection("Orders").doc(req.body.order_id).delete().then(function() {
+            res.render("message_page",{"message": " order rejected!"}) //can't redirect to the same page since then I will need more data.
+            //and nobody deletes so many housing units in a row.
+        }).catch(function(error) {
+            res.render("message_page",{"message" : error}) //should work with the back button.
+        })
+
+    })
+
+    app.post("/homepage_renter/add_attraction",body_json, (req,res) => {
+        database.collection("Attractions").add(req.body).then(function() {
+            res.send("Attraction added successfully!") //can't redirect to the same page since then I will need more data.
+            //and nobody deletes so many housing units in a row.
+        }).catch(function(error) {
+            res.send(error) //should work with the back button.
+        })
+
+    })
+
 
 
     app.post("/homepage_renter/accept_order",body_url, (req,res) => {
