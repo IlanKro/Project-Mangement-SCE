374119dae9151a50d53d579b3f898771256c4bbf
Ilan Kroter <44408485+IlanKro@users.noreply.github.com>
2020-06-15 13:22:57 +0300

    fixing a code smell

--- src/Utility/user.js
+++ src/Utility/user.js
@@ -1,12 +1,11 @@
 window.onload=function(){
     auth.onAuthStateChanged(function(user) {
-        if (user && auth.currentUser ) {
-            //if there is a welcome id it changes the text to logged user.
-            if (document.getElementById("welcome")) {
-                document.getElementById("welcome").innerHTML = "Logged in as : " + auth.currentUser.displayName
-            }
-        }
-        else
+        if (!(user && auth.currentUser)) {
             window.location= "/" //if no user is logged on kicks you out to index.
+            //if there is a welcome id it changes the text to logged user.
+        }
+        if (document.getElementById("welcome")) {
+            document.getElementById("welcome").innerHTML = "Logged in as : " + auth.currentUser.displayName
+        }          
     })
 }
